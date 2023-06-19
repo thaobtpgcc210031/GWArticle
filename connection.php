a14c04bdba61fa686938d546b6ad59432f387ad0
nguyendainghiep <nghiepndgcc210008@fpt.edu.vn>
2023-06-19 14:53:49 +0700

    nghiep

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
-    $conn = mysqli_connect('localhost', 'root', '', 'shirt_store')
-                            or die("Cannot connect database".mysqli_connect_error());
-    //$conn = mysqli_connect("host", "username", "pass". "database")
-    //or die("Cannot connect database".mysqli_connect_error());
+    //$conn = mysqli_connect('localhost', 'root', '', 'shirt_store')
+                            //or die("Cannot connect database".mysqli_connect_error());
+    $conn = mysqli_connect("host", "username", "pass". "database")
+    or die("Cannot connect database".mysqli_connect_error());
 ?>
