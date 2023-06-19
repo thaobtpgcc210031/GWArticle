f25aaec430bfb779b7f90ce761ebc1bbf42166b7
nguyendainghiep <nghiepndgcc210008@fpt.edu.vn>
2023-06-19 14:57:44 +0700

    nghiep

--- connection.php
+++ connection.php
@@ -3,4 +3,5 @@
                             //or die("Cannot connect database".mysqli_connect_error());
     $conn = mysqli_connect("host", "username", "pass". "database")
     or die("Cannot connect database".mysqli_connect_error());
+    
 ?>
