3088314c0f234a60c2501063176287e59f05fd66
nguyendainghiep <nghiepndgcc210008@fpt.edu.vn>
2023-06-23 16:23:04 +0700

    main

--- connection.php
+++ connection.php
@@ -1,12 +1,12 @@
 <?php
-    $conn = mysqli_connect('localhost', 'root', '', 'shirt_store')
-                            or die("Cannot connect database".mysqli_connect_error());
+    //$conn = mysqli_connect('localhost', 'root', '', 'shirt_store')
+                           // or die("Cannot connect database".mysqli_connect_error());
        
                             
 
-                            
-    // $conn = mysqli_connect("vkh7buea61avxg07.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    //  "ha9a4zq8qov6fifb", "k7no8xm2hkjbo61w", "oxjh34qgr0mcnz21")
-    // or die("Cannot connect database".mysqli_connect_error());
+
+     $conn = mysqli_connect("exbodcemtop76rnz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+     "dnxqqm2ow2ve1l75", "k9d5r7cxh8epjeon", "lm2ml3v53i4deuvg")
+     or die("Cannot connect database".mysqli_connect_error());
 
 ?>
