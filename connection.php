79147337815b82f2f2571899099f6ce6bf337bed
nguyendainghiep <nghiepndgcc210008@fpt.edu.vn>
2023-06-23 15:44:38 +0700

    main

--- connection.php
+++ connection.php
@@ -1,7 +1,10 @@
 <?php
     $conn = mysqli_connect('localhost', 'root', '', 'shirt_store')
                             or die("Cannot connect database".mysqli_connect_error());
-               
+       
+                            
+
+                            
     // $conn = mysqli_connect("vkh7buea61avxg07.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     //  "ha9a4zq8qov6fifb", "k7no8xm2hkjbo61w", "oxjh34qgr0mcnz21")
     // or die("Cannot connect database".mysqli_connect_error());
