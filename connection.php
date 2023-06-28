ff5858e2b1d5ea37ddcc3e8eb0bd2bbcfd71ced8
nguyendainghiep <nghiepndgcc210008@fpt.edu.vn>
2023-06-28 15:23:25 +0700

    main

--- connection.php
+++ connection.php
@@ -5,8 +5,8 @@
                             
 
 
-     $conn = mysqli_connect("exbodcemtop76rnz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-     "dnxqqm2ow2ve1l75", "k9d5r7cxh8epjeon", "lm2ml3v53i4deuvg")
+     $conn = mysqli_connect("m7az7525jg6ygibs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+     "a7h44jtfm1o6ytb9", "nnsinx3d42nncst4", "s8lvh0o6or1n9o09")
      or die("Cannot connect database".mysqli_connect_error());
 
 ?>
